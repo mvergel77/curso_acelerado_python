abc91400698d78607ddc41b9efed6d14690d30bb
Replit user <>
2022-08-09 16:44:50 +0000

    Actualización 5

---
+++ sesion2/Ejercicio_16.py
@@ -0,0 +1,14 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion2/ejercicio13.py
+Autor: ..............
+Action: imprime un triangulo
+'''
+n = int(input("Introduce un numero: "))
+
+for i in range(12):
+    print(str(n) + " * " + str(i+1) + " = " + str(n*(i+1)))
+    
+
+# Demo
